e2bb4ed585efa4a3d6cbfd2560123e513c5d028c
Chloe Yu <yuyaxi@uvic.ca>
2019-02-19 11:14:18 -0800

    avoid walls and self body

--- app/main.py
+++ app/main.py
@@ -1,10 +1,49 @@
+# 2019 COLO SNAKE
+# Author: Catherine, Chloe, Licht, Olivia
+# ----------------------------------------------------------------------------------------------------------
 import json
 import os
 import random
 import bottle
 
 from api import ping_response, start_response, move_response, end_response
+#------------------------------------------------methods----------------------------------------------
+#TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#get the direction options of my snake head (avoid walls, self body, others' bodies, longer snakes' heads)
+def next_direction_options(data):
+    directions = []
+    print(data["you"], "heihei")
+    my_head = data['you']['body'][0] #{"x": 4, "y": 4}
+    my_head_x = my_head['x']
+    my_head_y = my_head['y']
+    #check if we can move up
+    if (my_head['y']-1 >= 0):#avoid the wall
+        next_head_loc = {'x': my_head_x, 'y': my_head_y-1}
+        if ((next_head_loc in data['you']['body'][0:-1])==False):#avoid self bodies
 
+            directions.append('up')
+
+    #check if we can move right
+    if (my_head['x']+1 <= (data['board']['width']-1)):
+        next_head_loc = {'x': my_head_x+1, 'y': my_head_y}
+        if ((next_head_loc in data['you']['body'][0:-1])==False):
+            directions.append('right')
+
+    #check if we can move down
+    if (my_head['y']+1 <= (data['board']['height']-1)):
+        next_head_loc = {'x': my_head_x, 'y': my_head_y+1}
+        if ((next_head_loc in data['you']['body'][0:-1])==False):
+            directions.append('down')
+
+    #check if we can move left
+    if (my_head['x']-1 >= 0):
+        next_head_loc = {'x': my_head_x-1, 'y': my_head_y}
+        if ((next_head_loc in data['you']['body'][0:-1])==False):
+            directions.append('left')
+    return directions;
+
+
+#-------------------------------------------API calls------------------------------------------------------
 @bottle.route('/')
 def index():
     return '''
@@ -41,7 +80,7 @@ def start():
     """
     print(json.dumps(data))
 
-    color = "#00FF00"
+    color = "#FF69B4"
 
     return start_response(color)
 
@@ -55,8 +94,10 @@ def move():
             snake AI must choose a direction to move in.
     """
     print(json.dumps(data))
+    print(data)
 
-    directions = ['up', 'down', 'left', 'right']
+    # directions = ['up', 'down', 'left', 'right']
+    directions = next_direction_options(data)
     direction = random.choice(directions)
 
     return move_response(direction)
@@ -84,3 +125,9 @@ if __name__ == '__main__':
         port=os.getenv('PORT', '8080'),
         debug=os.getenv('DEBUG', True)
     )
+
+
+
+
+
+
